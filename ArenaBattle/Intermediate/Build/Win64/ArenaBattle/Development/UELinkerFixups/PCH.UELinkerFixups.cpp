e3b816f7fe3f54ae9e02b54a9ed81641f94ef24a
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2022-02-20 23:02:23 +0900

    Chapter1&2

---
+++ ArenaBattle/Intermediate/Build/Win64/ArenaBattle/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
@@ -0,0 +1,1 @@
+#include "D:/UE4CPP/ArenaBattle/Intermediate/Build/Win64/ArenaBattle/Development/UELinkerFixups/PCH.UELinkerFixups.h"
---
+++ ArenaBattle/Source/ArenaBattle/Fountain.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Fountain.h"
+
+
+// Sets default values
+AFountain::AFountain()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//컴포넌트를 생성
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY")); //언리얼 엔진은 new가 아닌 CreateDefaultSubobject를 사용
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
+	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
+	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+
+	//Body를 RootComponent로 지정하고, Water를 Body의 자식으로 설정
+	RootComponent = Body;
+	Water->SetupAttachment(Body);
+	Light->SetupAttachment(Body);
+	Splash->SetupAttachment(Body);
+
+	//위치를 설정
+	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 135.0f));
+	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01"));
+	if (SM_BODY.Succeeded())
+	{
+		Body->SetStaticMesh(SM_BODY.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_WATER(TEXT("StaticMesh'/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02'"));
+	if (SM_WATER.Succeeded())
+	{
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem>
+		PS_SPLASH(TEXT("'/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01'"));
+
+	if (PS_SPLASH.Succeeded())
+	{
+		Splash->SetTemplate(PS_SPLASH.Object);
+	}
+
+
+}
+
+// Called when the game starts or when spawned
+void AFountain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFountain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
